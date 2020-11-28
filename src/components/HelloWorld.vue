8a40565ae9700b559aa0b7af0fa75ebaf760356e
gbing04 <goran.bingula99@gmail.com>
2020-11-28 20:49:35 +0100

    1
    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <table style="width:100%">
+  <tr>
+    <th>DOMACI</th>
+    <th>REZULTAT</th>
+    <th>GOSTI</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
